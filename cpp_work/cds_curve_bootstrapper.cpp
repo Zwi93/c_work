1a4aefa132593e4cbf139ce10c9398160fa1b6bf
Zwimangadzo Mudau <ZWA@rmb.co.za>
2020-06-27 15:43:48 +0200

    Another update on 2020/06/27.

--- cpp_work/cds_curve_bootstrapper.cpp
+++ cpp_work/cds_curve_bootstrapper.cpp
@@ -1,15 +1,22 @@
-/*
- * Basic CDS curve bootstrapper. Script takes the CDS curve and calculates the risk-neutral survival probabilities.
+/************************************************************************************************************************************
+ * Purpose: Basic CDS curve bootstrapper. Script takes the CDS curve and calculates the risk-neutral survival probabilities.        * 
+ *                                                                                                                                  *
+ * Author: Zwi Mudau                                                                                                                *
+ *                                                                                                                                  *
+ * Usage: Run the program and it will print out the bootstrapped survival probabilities.                                            *
+ *                                                                                                                                  *
+ ************************************************************************************************************************************
  */
 
+//Include all relevant header files here.
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-//Function prototypes here.
+//Declare function prototypes here.
 void bond_curve_bootstrapper (double zero_disc_factor[6]);
 double bond_price_from_yield (double yield, double coupon_rate, double par_value, int bond_maturity);
-void cds_curve_bootstrapper ();
+void cds_curve_bootstrapper (double survival_probability[6], double LGD);
 
 //Define a new type to hold data for a certain CDS contract.
 struct cds_contract 
@@ -30,18 +37,30 @@ struct coupon_bonds
 
 int main ()
 {
+    //Initiate input variables.
+    double survival_probability[6];
+    double LGD = 0.4;
+
     //Bootstrapp cds curve.
-    cds_curve_bootstrapper();
+    cds_curve_bootstrapper(survival_probability, LGD);
 
     return 0;
 }
 
-void cds_curve_bootstrapper ()
+/************************************************************************************************************************************
+ * Purpose: Function to bootstrapp the cds curve from given input data.                                                             * 
+ *                                                                                                                                  *
+ * Author: Zwi Mudau                                                                                                                *
+ *                                                                                                                                  *
+ * Parameters:                                                                                                                      *
+ * survival-probability: Array to store the implied survival probabilities from the bootstrapp.                                     *
+ * LGD : Loss given default value                                                                                                   *
+ * Return: This is a void function.                                                                                                 *                                                                                                                                  
+ ************************************************************************************************************************************
+ */
+void cds_curve_bootstrapper (double survival_probability[6], double LGD = 0.4)
 {
-/*
-Function to perform bootstrapping of cds curve from data available in the market.
-*/
-    // Create array to carry the information of cds contracts to be bootstrapped.
+    // Create array of struct type (cds_contract) to carry the information of cds contracts to be bootstrapped.
     cds_contract cds_info[5];
     cds_info[0] = {"1Y cds", 100, 1.0};
     cds_info[1] = {"2Y cds", 114.00, 2.0};
@@ -50,12 +69,10 @@ Function to perform bootstrapping of cds curve from data available in the market
     cds_info[4] = {"5Y cds", 145.0, 5.0};
 
     //Obtain the bootstrapped zero yields from bonds' prices.
-    double risk_free_df[6];
+    double risk_free_df[6];  //Array to store the bootstrapped zero discount factors.
     bond_curve_bootstrapper(risk_free_df); //function to bootstrapp bond curve.
 
     //Initialize useful variable.
-    double survival_probability[6];
-    double LGD = 0.4;  // discount factor for the risk free lender. 
     int index;
 
     //Initialize array with 1s.
@@ -67,7 +84,6 @@ Function to perform bootstrapping of cds curve from data available in the market
     //Begin bootstrapping here.
     //First calculate the 1st implied survival probability.
     survival_probability[1] = LGD/(LGD + cds_info[0].cds_spread/pow(10, 4));
-    //cout << cds_info[0].cds_name + " survival probability\n" << survival_probability[0] << endl;
 
     //Then calculate the 2nd, 3rd etc...
     for (index = 2; index < 6; index++)
@@ -77,28 +93,35 @@ Function to perform bootstrapping of cds curve from data available in the market
 
         for (inner_index = 1; inner_index < index; inner_index++)
         {
-            temp_calc += risk_free_df[inner_index + 1]*(LGD*survival_probability[inner_index - 1] - (LGD + cds_info[inner_index].cds_spread/pow(10, 4))*survival_probability[inner_index]);
+            temp_calc += risk_free_df[inner_index]*(LGD*survival_probability[inner_index - 1] - (LGD + cds_info[index - 1].cds_spread/pow(10, 4))*survival_probability[inner_index]);
         }
-        cout << "summation " << temp_calc << endl;
 
         //spreads are divived by 10^4 for conversion to %.
-        survival_probability[index] = temp_calc/(risk_free_df[index + 1]*(LGD + cds_info[index].cds_spread/pow(10, 4))) + survival_probability[index - 1]*LGD/(LGD + cds_info[index].cds_spread/pow(10, 4)); 
+        survival_probability[index] = temp_calc/(risk_free_df[index]*(LGD + cds_info[index - 1].cds_spread/pow(10, 4))) + survival_probability[index - 1]*LGD/(LGD + cds_info[index - 1].cds_spread/pow(10, 4)); 
     }
 
+    //Print out the results.
     for (index = 1; index < 6; index++)
     {
         cout << cds_info[index - 1].cds_name + " survival probability\n" << survival_probability[index] << endl;
     }
 }
 
+/************************************************************************************************************************************
+ * Purpose: Function to bootstrapp the cds curve from given input data.                                                             * 
+ *                                                                                                                                  *
+ * Author: Zwi Mudau                                                                                                                *
+ *                                                                                                                                  *
+ * Parameters:                                                                                                                      *                                                                            
+ * zero_disc_factor: 1d array to store discount factors; passed as reference parameter.                                             *
+ *                                                                                                                                  *
+ * Return: This is a void function.                                                                                                 *                                                                                                                                  
+ ************************************************************************************************************************************
+ */
 void bond_curve_bootstrapper (double zero_disc_factor[6])
 {
-/*
-Function to perform bootstrapping of coupon bearing bonds from data available in the market.
-*/
     // Create array to carry the information of bonds to be bootstrapped.
     coupon_bonds bonds_info[5];
-    //bonds_info[0] = {"6m bond", 0.00185, 0.0, 0.5};
     bonds_info[0] = {"1Y bond", 0.00183, 0.0, 1.0};
     bonds_info[1] = {"2Y bond", 0.00199, 0.001, 2.0};
     bonds_info[2] = {"3Y bond", 0.00232, 0.0012, 3};
@@ -106,7 +129,6 @@ Function to perform bootstrapping of coupon bearing bonds from data available in
     bonds_info[4] = {"5Y bond", 0.00342, 0.0014, 5.0};
 
     //Initialize useful variable.
-    
     int index;
 
     //Initialize element 0 for zero_disc_factor array.
@@ -128,10 +150,12 @@ Function to perform bootstrapping of coupon bearing bonds from data available in
         }
 
         zero_disc_factor[index] = (current_bond_price - temp_factor)/(bonds_info[index - 1].coupon + 1);  //Face value is one for now.
-
-        cout << bonds_info[index - 1].bond_name + " zero factor\n" << zero_disc_factor[index] << endl;
     }
 
+    for (index = 1; index < 6; index++)
+    {
+        cout << bonds_info[index - 1].bond_name + " zero factor\n" << zero_disc_factor[index] << endl;
+    }
 }
 
 double bond_price_from_yield (double yield, double coupon_rate, double par_value, int bond_maturity)
