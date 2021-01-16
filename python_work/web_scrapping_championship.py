582ee6ea08e192546e01436bfb910f62a19c016b
Zwimangadzo <zwima93@gmail.com>
2021-01-16 19:57:18 +0200

    Another update on 2021/01/17, yes, a Saturday.

--- python_work/web_scrapping_championship.py
+++ python_work/web_scrapping_championship.py
@@ -6,6 +6,7 @@ Created on 2021/01/13 ....
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import bs4 as bs4
 import numpy as np
 import pandas as pd
 
@@ -16,8 +17,62 @@ html = urlopen(url_championship_2019_20)
 soup_object = BeautifulSoup(html, 'lxml')
 
 fixture_score = soup_object.find_all('div', 'fixres__item')
+#fixture_score = soup_object.find_all('a', 'matches__item matches__link')
 hidden_scores = soup_object.find_all('script', type='text/show-more')[0].string
 hidden_scores_soup_object = BeautifulSoup(hidden_scores, 'lxml')
-hidden_scores_another = hidden_scores_soup_object.find_all('div', 'fixres__item') 
+hidden_scores = hidden_scores_soup_object.find_all('div', 'fixres__item') 
 
-print(hidden_scores_another[0])
+#Add the two scores arrays; 1st convert them numpy arrays.
+#fixture_score = np.array(fixture_score)
+#hidden_scores = np.array(hidden_scores).flatten()
+#all_fixtures_scores = np.concatenate((fixture_score, hidden_scores)).flatten()
+#all_fixtures_scores = all_fixtures_scores
+#all_fixtures_scores = all_fixtures_scores[type(all_fixtures_scores) is bs4.element.Tag][0]
+
+"""
+for elem in all_fixtures_scores[:2]:#, all_fixtures_scores[201]):
+    #match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
+    home_team = elem.find_all('span', 'swap-text__target')[0].string
+    away_team = elem.find_all('span', 'swap-text__target')[1].string
+    match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+    match_score_away = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+
+    print('home: ', home_team)
+    print('away: ', away_team)
+    print('score: ', match_score_home)
+    print('score: ', match_score_away)
+
+"""
+for elem in fixture_score:
+    match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
+    html_match_stats = urlopen(match_link)
+    soup_obj = BeautifulSoup(html_match_stats, 'lxml')
+    match_date = soup_obj.find_all('time')[0].string
+    home_team = elem.find_all('span', 'swap-text__target')[0].string
+    away_team = elem.find_all('span', 'swap-text__target')[1].string
+    match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+    match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
+
+    print('home: ', home_team)
+    print('away: ', away_team)
+    print('score: ', match_score_home)
+    print('score: ', match_score_away)
+    print(match_date)
+
+for elem in hidden_scores:
+    match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
+    html_match_stats = urlopen(match_link)
+    soup_obj = BeautifulSoup(html_match_stats, 'lxml')
+    match_date = soup_obj.find_all('time')[0].string
+    home_team = elem.find_all('span', 'swap-text__target')[0].string
+    away_team = elem.find_all('span', 'swap-text__target')[1].string
+    match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+    match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
+
+    print('home: ', home_team)
+    print('away: ', away_team)
+    print('score: ', match_score_home)
+    print('score: ', match_score_away)
+    print(match_date)
+
+#print(hidden_scores)
