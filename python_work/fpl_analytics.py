86ad92fb26a03b9bbfcfc4a0dff1bc77beae7d4f
Zwimangadzo <zwima93@gmail.com>
2021-01-26 16:48:28 +0200

    another update on 2021/01/26.

--- python_work/fpl_analytics.py
+++ python_work/fpl_analytics.py
@@ -8,29 +8,54 @@ import requests as rq
 from fpl import FPL
 import aiohttp
 import asyncio
+import csv
 
 url = "https://fantasy.premierleague.com/api/element-summary/1/"
 url_1 = "https://fantasy.premierleague.com/api/bootstrap-static/"
 url_2 = "https://fantasy.premierleague.com/api/fixtures/?event=1"
 
-fpl_username = "zwima93@gmail.com"
-fpl_psswrd = "HXez.tzMcW?44,i"
 my_user_id = 4463037
 
 async def my_teams_performance (id):
+    "Function to assess FPL team performance throughout the season. Player's previous records are analyzed, and future points per game assessed"
     session = aiohttp.ClientSession()
     fpl = FPL(session)
 
     #Connect to my fpl account.
-    await fpl.login(fpl_username, fpl_psswrd)
-    this_user = await fpl.get_user(id)
-    my_team = await this_user.get_team()
+    fpl_username = "zwima93@gmail.com"
+    with open('/home/zwi/fpl_config_file.txt', 'r') as file1:
+        fpl_psswrd = ''
+        text_from_file = csv.reader(file1, delimiter=' ')
+        for line in text_from_file:
+            fpl_psswrd = fpl_psswrd + line[0]
+        
+        await fpl.login(fpl_username, fpl_psswrd)
+        this_user = await fpl.get_user(id)
+        my_team = await this_user.get_team()
 
-    for player in my_team:
-        player_id = player['element']
-        player_dict = await fpl.get_player(player_id, return_json=True)
-        print('points_per_game: ',player_dict['points_per_game'])
-        print('form: ', player_dict['form'])
-    await session.close()
+        for player in my_team:
+            player_id = player['element']
+            player_dict = await fpl.get_player(player_id, return_json=True)
+            player_team_id = player_dict['team']
+            players_team_obj = await fpl.get_team(player_team_id)
+
+            #Use the player's team to find his next 3 games.
+            next_fixtures = await players_team_obj.get_fixtures(return_json=False)
+            next_3_fixtures = next_fixtures[:3]
+
+            #Get player's summary.
+            player_summary = await fpl.get_player_summary(player_id, return_json=True)
+
+
+            print('Name: ', player_dict['second_name'])
+            print('points_per_game: ',player_dict['points_per_game'])
+            print('form: ', player_dict['form'])
+            print('Availability: ', player_dict['status'])
+            print('discipline (cards total): \n', player_dict['yellow_cards'] + player_dict['red_cards'])
+            print('player 3 previous games: \n', player_summary['history'][-3:])
+            print('player 3 next games: \n', player_summary['fixtures'][:3])
+            print('\n')
+
+        await session.close()
 
 asyncio.get_event_loop().run_until_complete(my_teams_performance(my_user_id))
--- python_work/web_scrapping_championship.py
+++ python_work/web_scrapping_championship.py
@@ -9,11 +9,14 @@ from bs4 import BeautifulSoup
 import bs4 as bs4
 import numpy as np
 import pandas as pd
+import csv
 
 url_championship = "https://www.skysports.com/championship-results"
 url_championship_2019_20 = "https://www.skysports.com/championship-results/2019-20"
+url_pl_2020_21 = "https://www.skysports.com/premier-league-results"
+url_pl_2019_20 = "https://www.skysports.com/premier-league-results/2019-20"
 
-html = urlopen(url_championship_2019_20)
+html = urlopen(url_pl_2019_20)
 soup_object = BeautifulSoup(html, 'lxml')
 
 fixture_score = soup_object.find_all('div', 'fixres__item')
@@ -43,36 +46,47 @@ for elem in all_fixtures_scores[:2]:#, all_fixtures_scores[201]):
     print('score: ', match_score_away)
 
 """
-for elem in fixture_score:
-    match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
-    html_match_stats = urlopen(match_link)
-    soup_obj = BeautifulSoup(html_match_stats, 'lxml')
-    match_date = soup_obj.find_all('time')[0].string
-    home_team = elem.find_all('span', 'swap-text__target')[0].string
-    away_team = elem.find_all('span', 'swap-text__target')[1].string
-    match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
-    match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
+with open('score_results_2019-20_pl.csv', 'w') as file1:
+    stream_to_file = csv.writer(file1, delimiter=',')
+    for elem in fixture_score[:2]:
+        match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
+        html_match_stats = urlopen(match_link, timeout=20)
+        soup_obj = BeautifulSoup(html_match_stats, 'lxml')
+        match_date = soup_obj.find_all('time')[0].string.split(',')[1]
+        home_team = elem.find_all('span', 'swap-text__target')[0].string
+        away_team = elem.find_all('span', 'swap-text__target')[1].string
+        match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+        match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
 
-    print('home: ', home_team)
-    print('away: ', away_team)
-    print('score: ', match_score_home)
-    print('score: ', match_score_away)
-    print(match_date)
+        stream_to_file.writerow([home_team, match_score_home, away_team, match_score_away, match_date])
+        #stream_to_file.writerow(home_team + ' ' + match_score_home + ' ' + away_team + ' ' + match_score_away + ' ' + match_date + '\n')
 
-for elem in hidden_scores:
-    match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
-    html_match_stats = urlopen(match_link)
-    soup_obj = BeautifulSoup(html_match_stats, 'lxml')
-    match_date = soup_obj.find_all('time')[0].string
-    home_team = elem.find_all('span', 'swap-text__target')[0].string
-    away_team = elem.find_all('span', 'swap-text__target')[1].string
-    match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
-    match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
+        #print('home: ', home_team)
+        #print('away: ', away_team)
+        #print('score: ', match_score_home)
+        #print('score: ', match_score_away)
+        #print(match_date)
 
-    print('home: ', home_team)
-    print('away: ', away_team)
-    print('score: ', match_score_home)
-    print('score: ', match_score_away)
-    print(match_date)
+    for elem in hidden_scores[:2]:
+        match_link = elem.find_all('a', 'matches__item matches__link')[0].get('href')
+        html_match_stats = urlopen(match_link, timeout=20)
+        soup_obj = BeautifulSoup(html_match_stats, 'lxml')
+        match_date = soup_obj.find_all('time')[0].string.split(',')[1]
+        home_team = elem.find_all('span', 'swap-text__target')[0].string
+        away_team = elem.find_all('span', 'swap-text__target')[1].string
+        match_score_home = elem.find_all('span', 'matches__teamscores-side')[0].string.strip()
+        match_score_away = elem.find_all('span', 'matches__teamscores-side')[1].string.strip()
+
+        stream_to_file.writerow([home_team, match_score_home, away_team, match_score_away, match_date])
+
+        #stream_to_file.writerow(home_team + ' ' + match_score_home + ' ' + away_team + ' ' + match_score_away + ' ' + match_date + '\n')
+
+        #print('home: ', home_team)
+        #print('away: ', away_team)
+        #print('score: ', match_score_home)
+        #print('score: ', match_score_away)
+        #print(match_date)
+
+#stream_to_file.close()
 
 #print(hidden_scores)
